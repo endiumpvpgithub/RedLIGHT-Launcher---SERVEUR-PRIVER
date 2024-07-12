eb2c81f846fb9980a942333132b754832984fcf6
endiumpvpgithub <64782483+endiumpvpgithub@users.noreply.github.com>
2024-07-13 01:42:27 +0200

    Big incomming

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Oh tu m'avais manqué...", "author": "Hugo" },
+            { "message": "On y va ?", "author": "Hugo" },
+            { "message": "C'est partie !", "author": "Hugo" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
